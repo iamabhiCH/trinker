6e4cd7633f1fd2219e2d2c181137e008e9a7541d
Abhishek Yadav <abhishekay442080@gmail.com>
2024-03-08 01:29:23 +0530

    Tailwind configuration done.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,10 @@ import MaxWidthWrapper from "@/components/maxWidthWrapper";
 
 export default function Home() {
   return (
-    <MaxWidthWrapper className="bg-red-500"></MaxWidthWrapper>
+    <MaxWidthWrapper className="mb-12 mt-28 sm:mt-40 flex flex-col items-center justify-center text-center">
+      <div className="mx-auto mb-4 flex max-w-fit items-center justify-center space-x-2 overflow-hidden rounded-full border border-gray-200 bg-white px-7 py-2 shadow-md backdrop-blur transition-all hover:border-gray-300 hover:bg-white/50">
+        <p className="text-sm font-semibold text-gray-700">Trinker is now public!</p>
+      </div>
+    </MaxWidthWrapper>
   );
 }
